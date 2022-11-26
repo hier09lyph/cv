d41e52e5408e66ab718dfb7de291f0aff592833a
hier09lyph <v.varona89@gmail.com>
2022-11-26 15:31:19 +0100

    refactor: change projekts

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function removeEveryOther(arr) {
     }
     return nArr
 }
+
+function solution(number) {
+    let sum = 0
+    for (let i = number - 1; i > 2; i--) {
+        (i % 5 && i % 3) ? 0: sum += i;
+    }
+    return sum
+}
